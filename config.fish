aeb9032c67d043f1839e56eaa3f49131759b8826
luoji <gunsluo@gmail.com>
2018-02-12 09:44:07 +0800

    fix set env to fish shell
    Signed-off-by: luoji <gunsluo@gmail.com>

--- config.fish
+++ config.fish
@@ -3,10 +3,10 @@ alias sshjpan="ssh -p 22000 root@172.104.127.173"
 alias sshme="ssh root@120.27.95.46"
 alias sshnl="ssh -p 22002 root@188.226.174.137"
 
-set GOROOT $HOME/go
-set GOPATH $HOME/gopath
-set GOBIN $GOPATH/bin
-set PATH $GOROOT/bin $GOPATH/bin $PATH
+set -g -x GOROOT $HOME/go
+set -g -x GOPATH $HOME/gopath
+set -g -x GOBIN $GOPATH/bin
+set -g -x PATH $GOROOT/bin $GOPATH/bin $PATH
 
 function setproxy
     export {http,https,ftp}_proxy="http://localhost:8016"
@@ -15,4 +15,3 @@ end
 function unsetproxy
     unset {http,https,ftp}_proxy
 end
-
