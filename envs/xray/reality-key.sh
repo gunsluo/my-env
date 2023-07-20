1ef735917362975561c9a1f7fcf9fea4118e79b5
luoji <gunsluo@gmail.com>
2023-07-20 10:20:42 +0800

    reality config
    Signed-off-by: luoji <gunsluo@gmail.com>

--- envs/xray/reality-key.sh
+++ envs/xray/reality-key.sh
@@ -7,4 +7,8 @@ xray x25519
 
 nerdctl run --rm -it teddysun/xray:1.8.1 xray uuid
 nerdctl run --rm -it teddysun/xray:1.8.1 xray x25519
+
+iptables -L
+iptables -A INPUT -p tcp --dport 33443 -j ACCEPT
+iptables -D INPUT -p tcp --dport 33443 -j ACCEPT
 ```
